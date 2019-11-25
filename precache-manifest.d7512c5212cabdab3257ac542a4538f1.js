77ae436563ea59bdc6bd4e43b218899e72a922ed
OlegBalabukh <olegukoff@gmail.com>
2019-11-25 18:53:10 +0200

    Updates

---
+++ precache-manifest.d7512c5212cabdab3257ac542a4538f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1243dd9b943d9275ee8241e603a0d941",
+    "url": "/cook-book/index.html"
+  },
+  {
+    "revision": "8f85906a4db6b96118aa",
+    "url": "/cook-book/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "ff845af8f6beb7498843",
+    "url": "/cook-book/static/css/main.522ad0e6.chunk.css"
+  },
+  {
+    "revision": "8f85906a4db6b96118aa",
+    "url": "/cook-book/static/js/2.a4e8eb95.chunk.js"
+  },
+  {
+    "revision": "ff845af8f6beb7498843",
+    "url": "/cook-book/static/js/main.b6b71fbb.chunk.js"
+  },
+  {
+    "revision": "d3608667efca75809926",
+    "url": "/cook-book/static/js/runtime-main.920f1877.js"
+  }
+]);
