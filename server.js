fdcd946ba9b0234df444fa7a000bc3451c6a1684
OlegBalabukh <olegukoff@gmail.com>
2019-12-05 01:32:35 +0200

    prepared for deployment

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set the static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
